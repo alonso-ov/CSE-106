cb938bb93c251626e9b3aad49a47ad588b78f1c0
alonso <alonsoortiz73@gmail.com>
2022-09-21 11:54:08 -0700

    progress on lab04

--- labs/lab04/script.js
+++ labs/lab04/script.js
@@ -1,3 +1,6 @@
+let var1 = 0
+let var2 = 0
+
 function append(val){
 
     let text = document.getElementById("output").value
@@ -9,4 +12,27 @@ function append(val){
 
 function cls(){
     document.getElementById("output").value = ''
+}
+
+function highlight(operator, unhighlight=false) {
+
+    if (unhighlight == true) {
+        document.getElementById(operator).style.backgroundColor = 'lightskyblue'
+        return
+    }
+
+    document.getElementById(operator).style.backgroundColor = 'gold'
+}
+
+function express(operator){
+    btns = document.getElementsByClassName('func')
+    Array.from(btns).forEach((elem) => {
+        if(elem.id != ''){
+            highlight(elem.id, true)
+        }
+    })
+
+    highlight(operator)
+
+    
 }
