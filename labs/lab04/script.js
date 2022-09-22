d6154154c071bab00637cc427e58c80ec0044699
alonso <alonsoortiz73@gmail.com>
2022-09-21 20:38:26 -0700

    added event listeners to buttons along with functionality to operation buttons still need some work before finished

--- labs/lab04/script.js
+++ labs/lab04/script.js
@@ -1,38 +1,72 @@
-let var1 = 0
-let var2 = 0
+
+const output = document.getElementById("output")
+const numbersButtons = document.querySelectorAll('.num')
+const operatorButtons = document.querySelectorAll('.oper')
+var currentInput = '', previousInput = ''
+var currentOperation, pastOperation
+
+function cls() {
+    output.value = ''
+    currentInput = ''
+    previousInput = ''
+}
 
 function append(val){
-
-    let text = document.getElementById("output").value
-
-    if (!(text.includes('.') & val == '.')){
-        document.getElementById("output").value += val
+    if(previousInput != '' & currentInput == ''){
+        output.value = ''
+    }
+    
+    if (!(output.value.toString().includes('.') & val == '.')){
+        output.value += val
+        currentInput = output.value
     }
 }
 
-function cls(){
-    document.getElementById("output").value = ''
-}
-
-function highlight(operator, unhighlight=false) {
-
-    if (unhighlight == true) {
-        document.getElementById(operator).style.backgroundColor = 'lightskyblue'
+function operation(operator) {
+    if (currentInput === '' ) {
         return
     }
-
-    document.getElementById(operator).style.backgroundColor = 'gold'
-}
-
-function express(operator){
-    btns = document.getElementsByClassName('func')
-    Array.from(btns).forEach((elem) => {
-        if(elem.id != ''){
-            highlight(elem.id, true)
-        }
-    })
-
-    highlight(operator)
-
     
+    if (previousInput !== '') {
+        solve(operator)
+    }
+
+    previousInput = output.value
+    currentInput = ''
 }
+
+function solve(operator){
+    const num1 = parseFloat(previousInput)
+    const num2 = parseFloat(currentInput)
+
+    switch(operator) {
+        case '+':
+            console.log('adding')
+            output.value = num1 + num2
+        case '-':
+            console.log('subtracting')
+            output.value = num1 - num2
+        case 'x':
+            console.log('multiplying')
+            output.value = num1 * num2
+        case '÷':
+            console.log('dividing')
+            output.value = num1 / num2
+        default:
+            return
+    }
+
+    previousInput = output.value
+    currentInput = ''
+}
+numbersButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        append(btn.innerText)
+    })
+})
+
+operatorButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        operation(btn.innerText)
+    })
+})
