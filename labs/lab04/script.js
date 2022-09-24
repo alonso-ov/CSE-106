4db21188ea5c1feb180ca495a0d9a13b9333d267
alonso <alonsoortiz73@gmail.com>
2022-09-23 18:21:43 -0700

    added math functionality

--- labs/lab04/script.js
+++ labs/lab04/script.js
@@ -2,17 +2,21 @@
 const output = document.getElementById("output")
 const numbersButtons = document.querySelectorAll('.num')
 const operatorButtons = document.querySelectorAll('.oper')
-var currentInput = '', previousInput = ''
-var currentOperation, pastOperation
+var currentInput = '', previousInput = '', currentOperation = ''
 
 function cls() {
     output.value = ''
     currentInput = ''
     previousInput = ''
+    currentOperation = ''
+    previousOperation = ''
 }
 
+
 function append(val){
-    if(previousInput != '' & currentInput == ''){
+
+    // if we have a past value and have nothing for our current then we must erase the output
+    if(previousInput != '' & currentInput.length == 0){
         output.value = ''
     }
     
@@ -23,41 +27,53 @@ function append(val){
 }
 
 function operation(operator) {
+
+    // if our current input in empty then return because we cannot do any math with empty values
     if (currentInput === '' ) {
         return
     }
-    
-    if (previousInput !== '') {
+
+    // at this point we are doing a math operation and need to fill our previous input
+    if (previousInput == '') {
+        previousInput = currentInput
+        currentInput = ''
+        currentOperation = operator
+    } else if (previousInput !== '') {
         solve(operator)
     }
-
-    previousInput = output.value
-    currentInput = ''
 }
 
 function solve(operator){
-    const num1 = parseFloat(previousInput)
-    const num2 = parseFloat(currentInput)
+    
+    let num1 = parseFloat(previousInput)
+    let num2 = parseFloat(currentInput)
+
 
     switch(operator) {
         case '+':
             console.log('adding')
             output.value = num1 + num2
+            break;
         case '-':
             console.log('subtracting')
             output.value = num1 - num2
+            break;
         case 'x':
             console.log('multiplying')
             output.value = num1 * num2
+            break;
         case '÷':
             console.log('dividing')
             output.value = num1 / num2
+            break;
+        case '=':
+            solve(currentOperation)
+            break;
         default:
-            return
+            break;
     }
 
     previousInput = output.value
-    currentInput = ''
 }
 numbersButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
