9dcafc4f6d67f5041a81ea9d83615012674eab80
alonso-ov <alonsoortiz73@gmail.com>
2022-10-23 13:28:04 -0700

    added creating new student feature to db

--- labs/lab07/app.py
+++ labs/lab07/app.py
@@ -14,16 +14,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 
-class Gradebook(db.Model):
+class Student(db.Model):
     name = db.Column(db.String, primary_key=True)
     grade = db.Column(db.String, nullable=False)
 
-
-#not necessary but if machine throws error
-#CORS(app)
-
-
-'''
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,40 +25,10 @@ def index():
 # create student
 @app.route('/grades', methods=['POST'])
 def post_student():
-    data[request.json['name']] = request.json['grade']
+    newStudent = Student(name=request.json['name'], grade=request.json['grade'])
+    db.session.add(newStudent)
+    db.session.commit()
     return 'success'
-
-#edit grade
-@app.route('/grades/<string:name>', methods=['PUT'])
-def put_student(name):
-    data[name] = request.json['grade']
-    return 'success'
-
-# get all students grade
-@app.route('/grades', methods=['GET'])
-def get_all_students():
-    return json.dumps(data)
-
-#get one student's grade
-@app.route('/grades/<string:name>', methods=['GET'])
-def get_student(name):
-    if name in data:
-        dict = {name: data[name]}
-        return json.dumps(dict)
-    else:
-        return 404
-
-# delete one student
-@app.route('/grades/<string:name>', methods=['DELETE'])
-def delete_student(name):
-    if name in data:
-        del data[name]
-        return 'successfully deleted'
-    else:
-        return 404
-
 
 if __name__ == '__main__':
     app.run()
-
-'''
