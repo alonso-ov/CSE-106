086564821549c224f36bde7a2e2f17ead7a8ec8b
alonso-ov <alonsoortiz73@gmail.com>
2022-10-23 12:53:45 -0700

    progress on lab07

--- labs/lab07/app.py
+++ labs/lab07/app.py
@@ -3,14 +3,27 @@ from flask import request
 from flask import abort, render_template
 from flask_cors import CORS
 
+from flask_sqlalchemy import SQLAlchemy
+
 import json
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3' 
+
+db = SQLAlchemy(app)
+
+class Gradebook(db.Model):
+    name = db.Column(db.String, primary_key=True)
+    grade = db.Column(db.String, nullable=False)
+
+
 #not necessary but if machine throws error
 #CORS(app)
-data = {}
 
+
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -50,5 +63,8 @@ def delete_student(name):
     else:
         return 404
 
+
 if __name__ == '__main__':
     app.run()
+
+'''
